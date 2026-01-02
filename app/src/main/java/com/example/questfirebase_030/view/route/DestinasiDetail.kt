fb3f1058d4d944c863a40b3bd37764b36ccaa605
ASUS <padiyogaradhana@gmail.com>
2026-01-02 18:58:40 +0700

    DestinasiDetail.kt

--- app/src/main/java/com/example/questfirebase_030/view/route/DestinasiDetail.kt
+++ app/src/main/java/com/example/questfirebase_030/view/route/DestinasiDetail.kt
@@ -2,7 +2,7 @@ package com.example.questfirebase_030.view.route
 
 import com.example.questfirebase_030.R
 
-object DstinasiDetail: DestinasiNavigasi {
+object DestinasiDetail: DestinasiNavigasi {
     override val route = "item_detail"
     override val titleRes = R.string.detail_siswa
     const val itemIdArg = "itemId"
