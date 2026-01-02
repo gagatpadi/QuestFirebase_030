4c7d3071278907afe9dc2e335de54c949d365cba
ASUS <padiyogaradhana@gmail.com>
2026-01-02 18:58:18 +0700

    DestinasiHome.kt

--- app/src/main/java/com/example/questfirebase_030/view/route/DestinasiHome.kt
+++ app/src/main/java/com/example/questfirebase_030/view/route/DestinasiHome.kt
@@ -1,7 +1,7 @@
 import com.example.questfirebase_030.R
 import com.example.questfirebase_030.view.route.DestinasiNavigasi
 
-object Destinasiome : DestinasiNavigasi {
+object DestinasiHome : DestinasiNavigasi {
     override val route = "home"
     override val titleRes = R.string.app_name
 }
