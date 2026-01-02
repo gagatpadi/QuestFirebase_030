ddf9fafb8b1c5e0fe4414856442aeb124bbb92cc
ASUS <padiyogaradhana@gmail.com>
2026-01-02 19:01:16 +0700

    EntryViewModel.kt

--- app/src/main/java/com/example/questfirebase_030/viewmodel/EntryViewModel.kt
+++ app/src/main/java/com/example/questfirebase_030/viewmodel/EntryViewModel.kt
@@ -19,6 +19,7 @@ class EntryViewModel(private val repositorySiswa: RepositorySiswa): ViewModel() 
         }
     }
 
+
     fun updateUiState(detailSiswa: DetailSiswa) {
         uiStateSiswa =
             UIStateSiswa(detailSiswa = detailSiswa, isEntryValid = validasiInput
